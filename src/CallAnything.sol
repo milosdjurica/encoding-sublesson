00eec3b8c179b1ac7b959bd3e2a26c5e0ac25ce2
milosdjurica <milosdjurica13@gmail.com>
2023-12-02 14:53:00 +0100

    callTransferFunctionWithBinary

--- src/CallAnything.sol
+++ src/CallAnything.sol
@@ -13,4 +13,22 @@ contract CallAnything {
     function getSelectorOne() public pure returns (bytes4 selector) {
         selector = bytes4(keccak256(bytes("transfer(address,uint)")));
     }
+
+    function getDataToCallTransfer(
+        address someAddress,
+        uint amount
+    ) public pure returns (bytes memory) {
+        return abi.encodeWithSelector(getSelectorOne(), someAddress, amount);
+    }
+
+    function callTransferFunctionWithBinary(
+        address someAddress,
+        uint amount
+    ) public returns (bytes4, bool) {
+        (bool success, bytes memory returnData) = address(this).call(
+            // getDataToCallTransfer(someAddress, amount)
+            abi.encodeWithSelector(getSelectorOne(), someAddress, amount)
+        );
+        return (bytes4(returnData), success);
+    }
 }
